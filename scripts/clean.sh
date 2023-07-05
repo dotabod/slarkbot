587139f787b62b96c428b9a36a421fa38100a927
Matt Gates <Geczy@users.noreply.github.com>
2023-07-05 09:49:44 -0500

    dont need these

--- scripts/clean.sh
+++
@@ -1,6 +0,0 @@
-
-echo "Cleaning top level directories"
-rm -rf logs .pytest_cache __pycache__ 
-
-echo "Cleaning Pycache"
-pyclean -v .
--- scripts/restart_bot.sh
+++
@@ -1,5 +0,0 @@
-echo "killing previous session"
-tmux kill-session -t slarkbot
-echo "starting new session"
-tmux new-session -d -s slarkbot ./scripts/start_bot.sh
-echo "started new session"
