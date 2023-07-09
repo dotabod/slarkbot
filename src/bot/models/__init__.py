337c1dba3a85285b0c1974c4d18873c3ec696782
Matt Gates <Geczy@users.noreply.github.com>
2023-07-09 11:17:32 -0500

    db url var

--- src/bot/models/__init__.py
+++ src/bot/models/__init__.py
@@ -7,14 +7,10 @@ Base = declarative_base()
 
 
 def create_database_engine():
-    db_host = os.getenv("POSTGRES_HOST")
-    db_user = os.getenv("POSTGRES_USER")
-    db_password = os.getenv("POSTGRES_PASSWORD")
-    db_port = os.getenv("DATABASE_PORT")
-    db_name = os.getenv("POSTGRES_DB")
+    db_url = os.getenv("POSTGRES_URL")
 
     connection_string = (
-        f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?sslmode=require"
+        f"{db_url}"
     )
     database_engine = create_engine(connection_string)
     return database_engine
